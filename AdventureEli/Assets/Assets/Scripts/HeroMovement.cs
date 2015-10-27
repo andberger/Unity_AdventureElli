4d8cf394bc0b6cf3c0a2cfde9fe7b272cfd99ed4
andberger <and.bergsson@gmail.com>
2015-10-27 20:52:26 +0100

    Added Initial Project

---
+++ AdventureEli/Assets/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMovement : MonoBehaviour {
+	
+	public float speed = 9.5f;
+	public float climbSpeed = 0.5f;
+	public float jumpHeight = 10f;
+	public bool mayJump = false;
+	public bool onWall = false;
+	
+	Vector2 movement;
+	Rigidbody playerRigidbody;
+	// Use this for initialization
+	void Awake () {
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw("Horizontal");
+		Move(h);
+	}
+	
+	void Move(float h){
+		movement.Set(h,0f);
+		movement = movement.normalized * speed* Time.deltaTime;
+
+
+		playerRigidbody.MovePosition (new Vector2(transform.position.x, transform.position.y) + movement);
+	}
+
+}
+
