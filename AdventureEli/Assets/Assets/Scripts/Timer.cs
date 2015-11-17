5370c5dbf25795b3acb0d9a8cc6eae1b9276f2eb
Marios A <argyrioumarios@gmail.com>
2015-11-17 15:48:51 +0100

    Timer
    timer added

---
+++ AdventureEli/Assets/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+	float counter;
+	public GUIStyle timerStyle;
+	
+	// Use this for initialization
+	void Start () {
+		timerStyle.fontSize = 35;
+	}
+	
+	void Awake(){
+		counter = 0f;
+		//	text = GetComponent <Timer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += Time.deltaTime;
+	}
+	
+	void OnGUI(){
+		if (counter >= 0f) {
+			GUI.Label(new Rect(350,24,100,100),"Time:" + (int)counter,timerStyle);
+		}
+		
+	}
+}
